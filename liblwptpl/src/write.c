2642378e93c564f94a5fded1324def6e58c69eac
Stefan Schuermans <stefan@schuermans.info>
2020-05-17 20:16:46 +0200

    add header to events to separate them

--- liblwptpl/src/write.c
+++ liblwptpl/src/write.c
@@ -1,13 +1,19 @@
 #include "write.h"
 
 #include <fcntl.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/file.h>
 #include <unistd.h>
 
+struct lwptpl_event_header_s {
+  uint8_t magic[4]; /**< l w p t */
+  uint8_t size[4]; /**< size of payload in network byte oder */
+} __attribute__((packed));
+
 void lwptpl_write(void const *data, size_t size) {
-  if (! data || ! size) {
+  if (! data || ! size || size > 0xFFFFFFFF) {
     return;
   }
   char const *filename = getenv("LWPTPL_OUTPUT");
@@ -22,6 +28,16 @@ void lwptpl_write(void const *data, size_t size) {
     close(fd);
     return;
   }
+  struct lwptpl_event_header_s lwptpl_event_header = {
+    .magic = { 'l', 'w', 'p', 't' },
+    .size = {
+      (size >> 24) & 0xFF,
+      (size >> 16) & 0xFF,
+      (size >> 8) & 0xFF,
+      size & 0xFF,
+    }
+  };
+  write(fd, &lwptpl_event_header, sizeof(lwptpl_event_header));
   write(fd, data, size);
   flock(fd, LOCK_UN);
   close(fd);
