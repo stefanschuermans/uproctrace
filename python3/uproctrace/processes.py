0f21276979b06fc561b2b359edaa232e3909ccbd
Stefan Schuermans <stefan@schuermans.info>
2020-05-23 17:36:45 +0200

    connect parent/child processes

--- python3/uproctrace/processes.py
+++ python3/uproctrace/processes.py
@@ -17,18 +17,32 @@ class Process():
         self._pid = pid
         self._begin = None
         self._end = None
+        self._parent = None
+        self._children = list()
+
+    def addChild(self, child: Process):
+        """
+        Add a child process.
+        """
+        self._children.append(child)
 
     def setBegin(self, proc_begin: uproctrace.parse.ProcBegin):
         """
-        set begin event of process
+        Set begin event of process.
         """
         self._begin = proc_begin
 
     def setEnd(self, proc_end: uproctrace.parse.ProcEnd):
         """
-        set end event of process
+        Set end event of process.
         """
         self._end = proc_end
+
+    def setParent(self, parent: Process):
+        """
+        Set parent process.
+        """
+        self._parent = parent
 
 
 class Processes(uproctrace.parse.Visitor):
@@ -41,7 +55,7 @@ class Processes(uproctrace.parse.Visitor):
         Initialize processes from a trace file (f).
         """
         super().__init__()
-        self._processes = dict()  # pid -> Process
+        self._current_processes = dict()  # pid -> Process
         self._readTrace(proto_file)
 
     def _readTrace(self, proto_file):
@@ -58,20 +72,25 @@ class Processes(uproctrace.parse.Visitor):
         # new process
         proc = Process(proc_begin.pid)
         # add process to dict of current processes
-        self._processes[proc_begin.pid] = proc
+        self._current_processes[proc_begin.pid] = proc
         # set begin event of process
         proc.setBegin(proc_begin)
+        # connect to parent
+        if proc_begin.ppid in self._current_processes:
+            parent = self._current_processes[proc_begin.ppid]
+            proc.setParent(parent)
+            parent.addChild(proc)
 
     def visitProcEnd(self, proc_end: uproctrace.parse.ProcEnd):
         """
         Process a process end event.
         """
         # get process (or create it it is not known)
-        if proc_end.pid in self._processes:
-            proc = self._processes[proc_end.pid]
+        if proc_end.pid in self._current_processes:
+            proc = self._current_processes[proc_end.pid]
         else:
             proc = Process(proc_end.pid)
         # set end event of process
         proc.setEnd(proc_end)
         # remove process from dict of current processes (it ended)
-        self._processes[proc_end.pid] = None
+        self._current_processes[proc_end.pid] = None
