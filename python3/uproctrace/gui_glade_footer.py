1703d7427fa6c5015f16cd721367376977167628
Stefan Schuermans <stefan@schuermans.info>
2020-05-25 19:49:11 +0200

    fix: no header at top of footer

--- python3/uproctrace/gui_glade_footer.py
+++ python3/uproctrace/gui_glade_footer.py
@@ -1,5 +1,1 @@
-# UProcTrace: User-space Process Tracing
-# Copyright 2020: Stefan Schuermans, Aachen, Germany <stefan@schuermans.info>
-# Copyleft: GNU LESSER GENERAL PUBLIC LICENSE version 3 (see LICENSE)
-
 """
